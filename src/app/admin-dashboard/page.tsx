899a8293265b744c4411f5df65e472124030997e
Tose2514 <tobias.sederstrom@gmail.com>
2026-04-24 09:58:31 +0200

    doc: added  admin buttons (#1)

--- src/app/admin-dashboard/page.tsx
+++ src/app/admin-dashboard/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 
 export default function AdminDashboard(){
     return(
         <div>
             <h1>Admin Dashboard</h1>
+            <ul>
+                <li>
+                    <Button><Link href="/admin-dashboard/add-movie">Add Movie</Link></Button>
+                </li>
+                <li>
+                    <Button><Link href="/admin-dashboard/add-movie">Change Movie</Link></Button>
+                </li>
+            </ul>
         </div>
     )
 }
