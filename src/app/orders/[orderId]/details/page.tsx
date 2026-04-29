1933bc4710c6f4585bd93c28376bb2cb48e0ff1a
Tose2514 <tobias.sederstrom@gmail.com>
2026-04-29 10:28:47 +0200

    order/order-details pages (#15)

---
+++ src/app/orders/[orderId]/details/page.tsx
@@ -0,0 +1,72 @@
+import prisma from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default async function OrderDetailsPage(
+  props: PageProps<"/orders/[orderId]/details">,
+) {
+  const params = await props.params;
+
+  const order = await prisma.order.findUnique({
+    where: { id: params.orderId },
+
+    include: { orderItem: { include: { movies: true } } },
+  });
+
+  if (!order) {
+    return notFound();
+  }
+  return (
+    <div className="flex-row max-w-3xl mx-auto border rounded-2xl p-4">
+      <Table>
+        <TableCaption>Order items</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-25">Item ID</TableHead>
+            <TableHead>Movie Title</TableHead>
+            <TableHead>Item Price</TableHead>
+            <TableHead>Quantity</TableHead>
+            <TableHead className="text-right">Movie ID</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {order.orderItem.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.movies.map((movie) => movie.title)}</TableCell>
+              <TableCell>{Number(item.priceAtPurchase)} SEK</TableCell>
+              <TableCell>{item.quantity}</TableCell>
+              <TableCell className="text-right text-blue-400">
+                <Link href={`/movies/${item.movies.map((movie) => movie.id)}`}>
+                  {item.movies.map((movie) => movie.id)}
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableCell>
+            <Button size="xs" asChild>
+              <Link href={`/orders/${order.id}`}>Back to order</Link>
+            </Button>
+          </TableCell>
+          <TableCell></TableCell> 
+          <TableCell></TableCell> 
+          <TableCell></TableCell> 
+          <TableCell></TableCell>
+        </TableFooter>
+      </Table>
+    </div>
+  );
+}
---
+++ src/app/orders/[orderId]/page.tsx
@@ -0,0 +1,62 @@
+import prisma from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+
+export default async function OrderPage(props: PageProps<"/orders/[orderId]">) {
+  const params = await props.params;
+
+  const order = await prisma.order.findUnique({
+    where: { id: params.orderId },
+  });
+
+  if (!order) {
+    return notFound();
+  }
+  return (
+    <div className="flex-row max-w-3xl mx-auto border rounded-2xl p-4">
+      <h1 className="text-4xl font-bold m-4">{`ID: ${order.id}`}</h1>
+      <Table>
+        <TableCaption>Order details</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Status</TableHead>
+            <TableHead>Order Date</TableHead>
+            <TableHead>Details</TableHead>
+            <TableHead>User ID</TableHead>
+            <TableHead className="text-right">Total</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell>{order.status}</TableCell>
+            <TableCell>{order.orderDate.toDateString()}</TableCell>
+            <TableCell className="text-blue-400">
+              <Link href={`/orders/${order.id}/details`}>Order details</Link>
+            </TableCell>
+            <TableCell>{order.userId}</TableCell>
+            <TableCell className="text-right">{order.totalAmount} SEK</TableCell>
+          </TableRow>
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell></TableCell> 
+            <TableCell></TableCell> 
+            <TableCell></TableCell> 
+            <TableCell></TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </div>
+  );
+}
