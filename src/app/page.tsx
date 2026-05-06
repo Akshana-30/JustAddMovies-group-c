0cbc824bf299e2e0327bb06ba27a3ca17039bf20
Per Sundström <per.f.sundstrom@gmail.com>
2026-05-06 20:53:28 +0200

    Synced carousel code
    I went into main and copied the existing  carousel code and pasted in my branch.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,56 +67,6 @@ async function LandingPage() {
       </Suspense>
 
       <div className="pt-2 overflow-hidden">
-        <Carousel className="pb- m-auto">
-          <CarouselContent>
-            {latestMovie.map((latest) => (
-              <CarouselItem key={latest.id}>
-                <MovBanner imageUrl={latest.imageUrl} className="shadow-2xl">
-                  <h1 className=" text-4xl font-bold p-5 min-h-200">
-                    {latest.title}
-
-                  </h1>
-                </MovBanner>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious variant="outline" className="absolute left-4 top-1/2 -translate-y-1/2 z-10" />
-          <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 z-10" />
-        </Carousel>
-
-        <Carousel
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-          className="max-w-full   "
-        >
-          <h1>Latest movies</h1>
-          <CarouselContent className="h-100">
-
-            {latestMovie.map((movies) => (
-              <CarouselItem key={movies.id} className="my-auto basis-1/2 lg:basis-1/6">
-                <div className="p-0">
-                  <div key={movies.id} className="flex justify-evenly gap-0">
-                    <MovieCard imageUrl={movies.imageUrl}>
-                      <Image
-                        src={movies.imageUrl}
-                        fill
-                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                        alt="image"
-                        className="mx-auto"
-                      />
-                    </MovieCard>
-                  </div>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious variant="outline" className="absolute left-4 top-1/2 -translate-y-1/2 z-10" />
-          <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 z-10" />
-        </Carousel>
-      </div>
-      <div className="pt-2 overflow-hidden">
         <div className="flex flex-row gap-10 p-4 sticky top-0 z-0">
           <div className="">
             {/* banner carousel */}
@@ -199,7 +149,7 @@ async function LandingPage() {
           <CarouselNext
             size="icon-lg"
             className="absolute right-5
-             top-1/2 "
+           top-1/2 "
           />
         </Carousel>
 
@@ -240,7 +190,7 @@ async function LandingPage() {
           <CarouselNext
             size="icon-lg"
             className="absolute right-5
-             top-1/2 "
+           top-1/2 "
           />
         </Carousel>
 
@@ -281,7 +231,7 @@ async function LandingPage() {
           <CarouselNext
             size="icon-lg"
             className="absolute right-5
-             top-1/2 "
+           top-1/2 "
           />
         </Carousel>
 
@@ -323,7 +273,7 @@ async function LandingPage() {
           <CarouselNext
             size="icon-lg"
             className="absolute right-5
-             top-1/2 "
+           top-1/2 "
           />
         </Carousel>
       </div>
