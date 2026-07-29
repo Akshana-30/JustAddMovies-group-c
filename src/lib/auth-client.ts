776d86c8a4c98cd3e77e09b5a5f0158dcb68424f
Lalitakshana R Elamurugu <lalitakshana@gmail.com>
2026-07-29 15:24:11 +0200

    neon better auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
+    baseURL: "https://just-add-movies-group-c-woad.vercel.app/",
     plugins: [
         adminClient(),
     ]
